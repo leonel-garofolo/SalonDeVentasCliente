34fc6c5f2e06d401c4609df7cd966b6df5499a9e
utn-rosario <garofolo.leonel@gmail.com>
2016-10-15 19:07:57 -0300

    descartar precio del producto
    descartar precio del producto

--- src/main/java/org/salondeventas/cliente/desktop/servicios/IPrecioproductoServicio.java
+++
@@ -1,6 +0,0 @@
-package org.salondeventas.cliente.desktop.servicios;
-
-import org.salondeventas.cliente.desktop.modelo.Precioproducto;
-
-public interface IPrecioproductoServicio extends IServicio<Precioproducto, java.lang.Long>{	
-}
